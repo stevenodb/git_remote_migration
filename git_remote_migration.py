7ec01e1ee1fd9601b945a9371e43cdb30b8c61ef
Steven Op de beeck <steven@opdebeeck.org>
2020-10-18 22:37:48 +0200

    Reorder functions

--- git_remote_migration.py
+++ git_remote_migration.py
@@ -21,16 +21,6 @@ COLOR_ATTRS = ['bold']
 COL_TRUNC = 40
 
 
-def is_git_repo(r: DirEntry) -> bool:
-    dot_git_path = os.path.join(r.path, '.git')
-    return os.path.isdir(dot_git_path)
-
-
-def scan_repositories(directory: AnyStr) -> List[DirEntry]:
-    with os.scandir(directory) as it:
-        return [r for r in it if r.is_dir() and is_git_repo(r)]
-
-
 def evaluate_arguments() -> Namespace:
     parser = ArgumentParser(description="Bulk migrate git remotes.")
     parser.add_argument("directory",
@@ -51,47 +41,13 @@ def evaluate_arguments() -> Namespace:
     return parser.parse_args()
 
 
-class GitRemoteTransform:
-    def __init__(self, repo_path, replace_pattern, replace_value) -> None:
-        super().__init__()
-        self.repo = Repo(path=repo_path)
-        self.pattern = re.compile(replace_pattern, re.IGNORECASE)
-        self.replace_value = replace_value
+def scan_repositories(directory: AnyStr) -> List[DirEntry]:
+    def is_git_repo(r: DirEntry) -> bool:
+        dot_git_path = os.path.join(r.path, '.git')
+        return os.path.isdir(dot_git_path)
 
-    def get_new_remotes(self) -> Dict[str, str]:
-        remotes = dict()
-        try:
-            for remote_url in self.repo.remote().urls:
-                new_remote_url = self.pattern.sub(self.replace_value, remote_url)
-                remotes[remote_url] = new_remote_url
-        except InvalidGitRepositoryError:
-            pass
-        return remotes
-
-    def add_as_row(self, table: Texttable) -> None:
-        for (old, new) in self.get_new_remotes().items():
-            key = truncate(os.path.basename(self.repo.working_dir), COL_TRUNC)
-            value = new + " " + colored("(unchanged)", color=YELLOW, attrs=COLOR_ATTRS) \
-                if old == new \
-                else colored(new, color=GREEN, attrs=COLOR_ATTRS)
-            table.add_row([key, value])
-
-    def transform(self):
-        for (old, new) in self.get_new_remotes().items():
-            print(f"- \"{os.path.basename(self.repo.working_dir)}\"... ", end='')
-            if old == new:
-                print(colored("Skipped", color=YELLOW, attrs=COLOR_ATTRS))
-            else:
-                try:
-                    self.repo.remote('origin').set_url(old_url=old, new_url=new)
-                    print(colored("Done", color=GREEN, attrs=COLOR_ATTRS))
-                except (ValueError, GitCommandError) as e:
-                    print(colored(f"Failed ({e})", color=RED, attrs=COLOR_ATTRS))
-
-
-def truncate(string: str, length: int) -> str:
-    dots = ".."
-    return string[:length - len(dots)] + dots if len(string) > length - len(dots) else string
+    with os.scandir(directory) as it:
+        return [r for r in it if r.is_dir() and is_git_repo(r)]
 
 
 def prepare_transformations(repo_dirs: List[DirEntry], pattern: str, replacement: str) -> List[GitRemoteTransform]:
@@ -140,6 +96,49 @@ def main():
         t.transform()
 
     print("Bye.")
+
+
+def truncate(string: str, length: int) -> str:
+    dots = ".."
+    return string[:length - len(dots)] + dots if len(string) > length - len(dots) else string
+
+
+class GitRemoteTransform:
+    def __init__(self, repo_path, replace_pattern, replace_value) -> None:
+        super().__init__()
+        self.repo = Repo(path=repo_path)
+        self.pattern = re.compile(replace_pattern, re.IGNORECASE)
+        self.replace_value = replace_value
+
+    def get_new_remotes(self) -> Dict[str, str]:
+        remotes = dict()
+        try:
+            for remote_url in self.repo.remote().urls:
+                new_remote_url = self.pattern.sub(self.replace_value, remote_url)
+                remotes[remote_url] = new_remote_url
+        except InvalidGitRepositoryError:
+            pass
+        return remotes
+
+    def add_as_row(self, table: Texttable) -> None:
+        for (old, new) in self.get_new_remotes().items():
+            key = truncate(os.path.basename(self.repo.working_dir), COL_TRUNC)
+            value = new + " " + colored("(unchanged)", color=YELLOW, attrs=COLOR_ATTRS) \
+                if old == new \
+                else colored(new, color=GREEN, attrs=COLOR_ATTRS)
+            table.add_row([key, value])
+
+    def transform(self):
+        for (old, new) in self.get_new_remotes().items():
+            print(f"- \"{os.path.basename(self.repo.working_dir)}\"... ", end='')
+            if old == new:
+                print(colored("Skipped", color=YELLOW, attrs=COLOR_ATTRS))
+            else:
+                try:
+                    self.repo.remote('origin').set_url(old_url=old, new_url=new)
+                    print(colored("Done", color=GREEN, attrs=COLOR_ATTRS))
+                except (ValueError, GitCommandError) as e:
+                    print(colored(f"Failed ({e})", color=RED, attrs=COLOR_ATTRS))
 
 
 if __name__ == '__main__':
